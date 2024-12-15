792b61ab929760211784ee464249aa9d96cebcdf
Javier <jcarrui476@g.educaand.es>
2024-12-15 15:11:15 +0100

    Esta es la buena

--- HolaMund.java
+++ HolaMund.java
@@ -1,7 +1,7 @@
 public class HolaMund {
     public static void main(String[] args) {
         System.out.println("Holaaaa");
-        System.out.println("lo he cambiado en remoto");
+        System.out.println("lo he cambiado en local");
     }
 }
 
