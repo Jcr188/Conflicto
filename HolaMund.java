ef15abba883b43dff163c4a711360be3c2035b0e
Javier <jcarrui476@g.educaand.es>
2024-12-15 14:38:44 +0100

    Update HolaMund.java

--- HolaMund.java
+++ HolaMund.java
@@ -1,6 +1,6 @@
 public class HolaMund {
     public static void main(String[] args) {
-        System.out.println("Hola puto");
+        System.out.println("Holaaaa");
         System.out.println("lo he cambiado");
     }
 }
